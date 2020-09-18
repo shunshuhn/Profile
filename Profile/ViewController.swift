625923896e67210372b3495b75eca09cb06a5c72
HARUKA ito <haruka_ito@keio.jp>
2020-09-18 13:56:52 +0900

    プロフィールアプリ応用の完成

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -12,23 +12,52 @@ class ViewController: UIViewController {
     
     @IBOutlet var profileImageView: UIImageView!
     @IBOutlet var profileCommentLabel: UILabel!
+    @IBOutlet var profileLabel: UILabel!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        profileImageView.isHidden = true
         
-        profileCommentLabel.isHidden = true
     }
 
     
     @IBAction func tapButton1() {
         
-        profileImageView.isHidden = false
+        profileImageView.image = UIImage(named: "philImage")
         
-        profileCommentLabel.isHidden = false
+        profileLabel.text = "名前"
+        
+        profileCommentLabel.text = "iPhoneメンターのフィルだよ"
+        
+    }
+    
+    @IBAction func tapButton2() {
+        
+        profileImageView.image = UIImage(named:"trackImage")
+        
+        profileLabel.text = "スポーツ"
+        
+        profileCommentLabel.text = "陸上競技が好きで走り幅跳びが得意"
+    }
+    
+    @IBAction func tapButton3() {
+        
+        profileImageView.image = UIImage(named:"appleImage")
+        
+        profileLabel.text = "好きな食べ物"
+        
+        profileCommentLabel.text = "おリンゴが好き"
+    }
+    
+    @IBAction func tapButton4() {
+        
+        profileImageView.image = UIImage(named:"flightImage")
+        
+        profileLabel.text = "趣味"
+        
+        profileCommentLabel.text = "スターフライヤーによく乗る"
     }
 }
 
